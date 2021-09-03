83cee94285e5801af962e2e9cd15de6c7f1136e9
bbcbbk <44605459+bbcbbk@users.noreply.github.com>
2021-09-03 23:10:14 +0800

    Update joycck.js

--- joycck.js
+++ joycck.js
@@ -118,8 +118,8 @@ async function joyReward() {
         // console.log(`宠物等级 ${data.level}\n`);
         let saleInfoId = '', giftValue = '', extInfo = '', leftStock = 0, salePrice = 0;
         let rewardNum = 0;
-        if ($.isNode() && process.env.JD_JOY_REWARD_NAME) {
-          rewardNum = process.env.JD_JOY_REWARD_NAME * 1;
+        if ($.isNode() && process.env.JD_JOYXXX) {
+          rewardNum = process.env.JD_JOYXXX * 1;
         } else if ($.getdata('joyRewardName')) {
           if ($.getdata('joyRewardName') * 1 === 1) {
             //兼容之前的BoxJs设置
